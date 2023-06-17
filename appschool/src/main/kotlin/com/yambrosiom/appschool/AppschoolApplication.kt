9edc17d51513378eef739d3b6f0a86e91292b168
yambrosio <YENER.AMBROSIO@TOMZAGROUP.COM>
2023-06-17 11:49:57 -0600

    Initial development project

---
+++ appschool/src/main/kotlin/com/yambrosiom/appschool/AppschoolApplication.kt
@@ -0,0 +1,11 @@
+package com.yambrosiom.appschool
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AppschoolApplication
+
+fun main(args: Array<String>) {
+	runApplication<AppschoolApplication>(*args)
+}
---
+++ appschool/src/test/kotlin/com/yambrosiom/appschool/AppschoolApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.yambrosiom.appschool
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AppschoolApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
